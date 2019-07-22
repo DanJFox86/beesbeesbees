25862ecb1353c5977cde00abe82fbc99623182f7
Dan Fox <fox.j.dan@gmail.com>
2019-07-22 11:34:08 -0700

    Moving from personal laptop to workstation

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
-var Grub = function() {
+var Grub = function(age = 0, color = 'pink') {
+
+	this.age = age;
+	this.color = color;
+	this.food = 'jelly';
 };
+
+Grub.prototype.eat = function() {
+	
+}
 
